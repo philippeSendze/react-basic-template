8bd59798a59303a0e43930ddd0ddc4c13090dee6
Philippe Sendze <sendze.philippe@gmail.com>
2023-01-28 06:44:02 +0100

    add a header and a footer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import GlobalStyle from "./style/GlobalStyle";
+import GlobalStyle from "./utils/style/GlobalStyle";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
@@ -21,6 +23,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
+    <Header />
     <RouterProvider router={router} />
+    <Footer />
   </React.StrictMode>
 );
